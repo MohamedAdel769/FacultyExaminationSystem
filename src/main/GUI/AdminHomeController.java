316003376499cf93547c65d1d56b979bafbc6f58
MohamedAdel <mohamed.adel90211@gmail.com>
2019-04-15 01:06:24 +0200

    Created GUIHelper class, Added input Validation

--- src/main/GUI/AdminHomeController.java
+++ src/main/GUI/AdminHomeController.java
@@ -1,4 +1,67 @@
 package main.GUI;
 
-public class AdminHomeController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.StackPane;
+import main.Users.Student;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.TableView;
+
+public class AdminHomeController implements Initializable {
+    GUIHelper guiHelper = new GUIHelper();
+    @FXML
+    TableView<Student> adminTableView;
+
+    @FXML
+    StackPane stackPane ;
+
+    @FXML
+    public void Logout(ActionEvent event){
+        guiHelper.GoToForm("Login.fxml");
+    }
+
+    @FXML
+    public void CreateSession(ActionEvent event){
+        guiHelper.ShowDialog(stackPane, "Examination Session", "You successfully added a new examination session.", "Ok");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        TableColumn<Student,String> idCol = new TableColumn<>("ID");
+        TableColumn<Student,String> nameCol = new TableColumn<>("Name");
+        TableColumn<Student,String> emailCol = new TableColumn<>("E-mail");
+        TableColumn<Student,String> selectCol = new TableColumn<>("Select");
+        nameCol.setCellValueFactory(new PropertyValueFactory("name"));
+        emailCol.setCellValueFactory(new PropertyValueFactory("email"));
+        idCol.setCellValueFactory(new PropertyValueFactory("id"));
+        selectCol.setCellValueFactory(new PropertyValueFactory("selectStd"));
+        idCol.setPrefWidth(100.0);
+        nameCol.setPrefWidth(150.0);
+        emailCol.setPrefWidth(200.0);
+        idCol.setResizable(false);
+        nameCol.setResizable(false);
+        emailCol.setResizable(false);
+        selectCol.setResizable(false);
+        selectCol.setStyle("-fx-alignment: center;");
+        adminTableView.getColumns().addAll(idCol,nameCol,emailCol,selectCol);
+        ObservableList<Student> items = FXCollections.observableArrayList(
+                new Student("mahmood","madfb@","50",new CheckBox()),
+                new Student("s sdv","madfb@","50",new CheckBox()),
+                new Student("sdvggb","madfb@","50",new CheckBox()),
+                new Student("tyrtu","madfb@","50",new CheckBox()),
+                new Student("io,im","madfb@","50",new CheckBox()),
+                new Student("wxedweff","madfb@","50",new CheckBox()),
+                new Student("mahmood","madfb@","50",new CheckBox()),
+                new Student("mahmood","madfb@","50",new CheckBox())
+        );
+        adminTableView.setItems(items);
+    }
 }
