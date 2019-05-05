3a8bc4e989311f0e698a997997d04e9cdf686c32
MohamedAdel <mohamed.adel90211@gmail.com>
2019-05-06 00:47:36 +0200

    Designed ExamSessions, Grades, StudentHome

---
+++ src/main/GUI/HistogramController.java
@@ -0,0 +1,15 @@
+package main.GUI;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HistogramController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
