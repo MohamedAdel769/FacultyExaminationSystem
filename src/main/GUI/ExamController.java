ceaf3a89b90ec411de52cda152065f116e5c449e
MohamedAdel <mohamed.adel90211@gmail.com>
2019-05-06 16:12:56 +0200

    Designed Exam form

---
+++ src/main/GUI/ExamController.java
@@ -0,0 +1,21 @@
+package main.GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ExamController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void Next(ActionEvent e){
+
+    }
+}
